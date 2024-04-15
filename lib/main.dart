c79191fb03f0f616195e3df18522ea9e627761c9
octav8us <octav8us@gmail.com>
2024-04-15 15:39:36 +0200

    - added get it package and initial implementation

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:mentorship/core/helpers/get_it_setup.dart';
 import 'package:mentorship/core/routing/app_router.dart';
 import 'package:mentorship/rockets_app.dart';
 
 void main() {
+  setupLocator();
   runApp(RocketsApp(
     appRouter: AppRouter(),
   ));
