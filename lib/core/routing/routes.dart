beca062444a6a6f0e6f110936d404a308754a476
Sara <s.hossam@nu.edu.eg>
2024-04-18 18:45:37 +0200

    - Added DefaultSearchBar and DefaultAppBar in sharedWidgets folder - Finish RocketsScreen UI and added (RocketsList + RocketsListItem)

--- lib/core/routing/routes.dart
+++ lib/core/routing/routes.dart
@@ -2,4 +2,6 @@ class Routes {
   static const String homeScreen = '/homeScreen';
   static const String splashScreen = '/splashScreen';
   static const String onBoardingScreen = '/onBoardingScreen';
+  static const String rocketsScreen = '/rocketsScreen';
+  static const String rocketDetailsScreen = '/rocketDetailsScreen';
 }
