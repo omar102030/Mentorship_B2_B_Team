75d210941c6f249c03eb035a70f110da56fba737
octav8us <octav8us@gmail.com>
2024-04-19 13:59:25 +0200

    Fx launcpad screen always requests the data after navigation

--- lib/features/launchpad/presentation/pages/launchpads_screen.dart
+++ lib/features/launchpad/presentation/pages/launchpads_screen.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:mentorship/features/launchpad/presentation/cubit/launchpad_state.dart';
 
-import '../../../../core/sharedWidgets/default_appbar.dart';
 import '../cubit/launchpad_cubit.dart';
 import '../widgets/launchpad_card.dart';
 
@@ -20,29 +19,31 @@ class _LaunchpadsScreenState extends State<LaunchpadsScreen> {
   void initState() {
     super.initState();
     _launchpadCubit = BlocProvider.of<LaunchpadCubit>(context);
-    _launchpadCubit.getLaunchpads();
+    if (_launchpadCubit.state is LaunchpadInitial) {
+      _launchpadCubit.getLaunchpads();
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return BlocBuilder<LaunchpadCubit, LaunchpadState>(
-        builder: (context, state) {
-          if (state is LaunchpadLoading) {
-            return const Center(child: CircularProgressIndicator());
-          } else if (state is LaunchpadLoaded) {
-            return ListView.builder(
-              itemCount: state.data.length,
-              itemBuilder: (context, index) {
-                return LaunchpadCard(
-                  launchpad: state.data[index],
-                );
-              },
-            );
-          } else if (state is LaunchpadError) {
-            return Center(child: Text(state.message));
-          }
-          return Container();
-        },
-      );
+      builder: (context, state) {
+        if (state is LaunchpadLoading) {
+          return const Center(child: CircularProgressIndicator());
+        } else if (state is LaunchpadLoaded) {
+          return ListView.builder(
+            itemCount: state.data.length,
+            itemBuilder: (context, index) {
+              return LaunchpadCard(
+                launchpad: state.data[index],
+              );
+            },
+          );
+        } else if (state is LaunchpadError) {
+          return Center(child: Text(state.message));
+        }
+        return Container();
+      },
+    );
   }
 }
